bdb0814bd87a103ac8b1de136290c5ba18e7d4ea
Felipe Spinolo <fspinol0@gmail.com>
2015-10-10 20:53:47 -0400

    Setup spec for Minimax, and skeleton for implementation

---
+++ ConnectR/ConnectR/MiniMax.cpp
@@ -0,0 +1,34 @@
+/* 
+ * Minimax Implementation for ConnectR
+ * Felipe Spinolo
+ * CS4523 Artificial Intelligence
+ * Dr. Michael Franklin
+ * Project 2
+ */
+
+#include "Minimax.h"
+
+unsigned int MiniMax::alphaBeta
+	(tree& root, unsigned int depth, int alpha, int beta, bool myTurn) {
+
+	// TODO: Minimax with alpha-beta pruning
+
+	return 0;
+
+}
+
+int MiniMax::scoreBoard(std::string& board) {
+
+	// TODO: Return a score for the given board
+
+	return 0;
+
+}
+
+bool MiniMax::hasKillerMove(std::string& board, bool myTurn) {
+
+	// TODO: Return whether there is a winning move for the current player
+
+	return false;
+
+}
--- ConnectR/ConnectR/Minimax.h
+++ ConnectR/ConnectR/Minimax.h
@@ -1,5 +1,5 @@
 /* 
- * Minimax with Alpha-Beta pruning
+ * Minimax with Alpha-Beta pruning for ConnectR
  * Felipe Spinolo
  * CS4523 Artificial Intelligence
  * Dr. Michael Franklin
@@ -9,28 +9,49 @@
 #ifndef _MINIMAX_H_
 #define _MINIMAX_H_
 
-#include <memory>
 #include <string>
 
 namespace MiniMax {
 
+	/* Representation of the current game state */
 	struct gameState {
-		unsigned int score;
-		bool hasWinningMove;
+		int score;
 		std::string board;
 	};
 
+	/* Tree structure used for minimaxing */
 	struct tree {
 		tree* children;
 		gameState state;
-		int alpha;
-		int beta;
 		tree() {}
-		tree(unsigned int m) { children = new tree[m]; }
+		tree(unsigned int m, gameState& s) {
+			children = new tree[m];
+			state = s;
+		}
 		~tree() { delete children; }
 	};
 
-	unsigned int alphaBeta(gameState& state);
+	/* Returns the index of the child node of root representing the best move
+	 * found.
+	 * @param root: tree representing current state
+	 * @param depth: how many moves to look ahead
+	 * @param alpha: the alpha value, start with a REALLY low value
+	 * @param beta: the beta value, start with a REALLY high value
+	 * @param myTurn: is it my turn at this depth? (should I min or max?)
+	 */
+	unsigned int alphaBeta
+		(tree& root, unsigned int depth, int alpha, int beta, bool myTurn);
+
+	/* Returns the integer score of the given board 
+	 * @param board: the board you want scored
+	 */
+	int scoreBoard(std::string& board);
+
+	/* Does the given board have a winning move for whoever's turn it is?
+	 * @param board: the board you want evaluated
+	 * @param myTurn: is it my turn? (my killer move or his?)
+	 */
+	bool hasKillerMove(std::string& board, bool myTurn);
 
 }
 
