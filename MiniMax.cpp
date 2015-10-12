c7e19d387a4f86b455165b2f9b04b4bd109e6c88
Felipe Spinolo <fspinol0@gmail.com>
2015-10-12 09:42:31 -0400

    fixed weird formatting characters from windows

--- MiniMax.cpp
+++ MiniMax.cpp
@@ -11,95 +11,97 @@
 
 const size_t MiniMax::alphaBeta(tree& root) {
 
-	/* Return the index of the best move we can find */
+    /* Return the index of the best move we can find */
 
-	int v = maximize( // currently looking 3 rounds (6 moves) ahead
-		root, 6, std::numeric_limits<int>::min(),
-		std::numeric_limits<int>::max()
-	);
-	// find the child that has score v
-	for (size_t i = 0; i < root.children.size(); ++i) {
-		if (root.children[i].score == v) {
-			return i;
-		}
-	}
-	// if all else fails, pick the middle
-	return root.children.size() / 2;
+    int v = maximize( // currently looking 3 rounds (6 moves) ahead
+        root, 6, std::numeric_limits<int>::min(),
+        std::numeric_limits<int>::max()
+    );
+    // find the child that has score v
+    for (size_t i = 0; i < root.children.size(); ++i) {
+        if (root.children[i].score == v) {
+            return i;
+        }
+    }
+    // if all else fails, pick the middle
+    return root.children.size() / 2;
 
 }
 
 const int MiniMax::maximize(tree& current, size_t depth, int alpha, int beta) {
 
-	/* Return the highest heuristic value from the moves available to us */
+    /* Return the highest heuristic value from the moves available to us */
 
-	if (winningMove(current.board, true)) { // Ouch.  Game over.
-		current.score = std::numeric_limits<int>::min();
-		return current.score;
-	}
+    if (winningMove(current.board, true)) { // Ouch.  Game over.
+        current.score = std::numeric_limits<int>::min();
+        return current.score;
+    }
 
-	if (depth == 0) { // Base case.  Run the heuristic.
-		current.score = scoreBoard(current.board);
-		return current.score;
-	}
-	// Recursive case.  Take the max of the mins.
-	int v = std::numeric_limits<int>::min();
-	size_t numChildren = current.children.size();
-	for (size_t i = 0; i < numChildren; ++i) {
-		std::vector<std::string> childBoard = makeMove(current.board, i, true);
-		current.children[i] = tree(numChildren, childBoard);
-		v = std::max(v, minimize(current.children[i], --depth, alpha, beta));
-		if (v >= beta) { // ***PRUNE***
-			return v;
-		}
-		alpha = std::max(alpha, v);
-	}
-	// We didn't prune.
-	return v;
+    if (depth == 0) { // Base case.  Run the heuristic.
+        current.score = scoreBoard(current.board);
+        return current.score;
+    }
+    // Recursive case.  Take the max of the mins.
+    int v = std::numeric_limits<int>::min();
+    size_t numChildren = current.children.size();
+    for (size_t i = 0; i < numChildren; ++i) {
+        std::vector<std::string> childBoard = makeMove(current.board, i, true);
+        current.children[i] = tree(numChildren, childBoard);
+        v = std::max(v, minimize(current.children[i], --depth, alpha, beta));
+        if (v >= beta) { // ***PRUNE***
+            return v;
+        }
+        alpha = std::max(alpha, v);
+    }
+    // We didn't prune.
+    return v;
 
 }
 
 const int MiniMax::minimize(tree& current, size_t depth, int alpha, int beta) {
 
-	/* Return the lowest heuristic value from the moves available to the opponent */
+    /* Return the lowest heuristic value from the moves available to the
+     * opponent */
 
-	if (winningMove(current.board, false)) { // Our move was a winner!
-		current.score = std::numeric_limits<int>::max();
-		return current.score;
-	}
+    if (winningMove(current.board, false)) { // Our move was a winner!
+        current.score = std::numeric_limits<int>::max();
+        return current.score;
+    }
 
-	if (depth == 0) { // Base case.  Run the heuristic.
-		current.score = scoreBoard(current.board);
-		return current.score;
-	}
-	// Recursive case.  Take the min of the maxes.
-	int v = std::numeric_limits<int>::max();
-	size_t numChildren = current.children.size();
-	for (size_t i = 0; i < numChildren; ++i) {
-		std::vector<std::string> childBoard = makeMove(current.board, i, false);
-		current.children[i] = tree(numChildren, childBoard);
-		v = std::min(v, maximize(current.children[i], --depth, alpha, beta));
-		if (v <= alpha) { // ***PRUNE***
-			return v;
-		}
-		beta = std::min(beta, v);
-	}
-	// We didn't prune.
-	return v;
+    if (depth == 0) { // Base case.  Run the heuristic.
+        current.score = scoreBoard(current.board);
+        return current.score;
+    }
+    // Recursive case.  Take the min of the maxes.
+    int v = std::numeric_limits<int>::max();
+    size_t numChildren = current.children.size();
+    for (size_t i = 0; i < numChildren; ++i) {
+        std::vector<std::string> childBoard = makeMove(current.board, i, false);
+        current.children[i] = tree(numChildren, childBoard);
+        v = std::min(v, maximize(current.children[i], --depth, alpha, beta));
+        if (v <= alpha) { // ***PRUNE***
+            return v;
+        }
+        beta = std::min(beta, v);
+    }
+    // We didn't prune.
+    return v;
 
 }
 
 const int MiniMax::scoreBoard(const std::vector<std::string>& board) {
 
-	// TODO: Return a score for the given board
+    // TODO: Return a score for the given board
 
-	return 0;
+    return 0;
 
 }
 
 const bool MiniMax::winningMove(const std::vector<std::string>& board, const bool myTurn) {
 
-	// TODO: Return true if the most recent move resulted in a win
+    // TODO: Return true if the most recent move resulted in a win
 
-	return false;
+    return false;
 
 }
+
--- MiniMax.h
+++ MiniMax.h
@@ -15,49 +15,52 @@
 
 namespace MiniMax {
 
-	/* Tree structure used for minimaxing */
-	struct tree {
-		std::vector<tree> children;
-		std::vector<std::string> board;
-		int score;
-		tree() {}
-		tree(size_t m, std::vector<std::string>& b) {
-			children.resize(m);
-			board = b;
-		}
-	};
+    /* Tree structure used for minimaxing */
+    struct tree {
+        std::vector<tree> children;
+        std::vector<std::string> board;
+        int score;
+        tree() {}
+        tree(size_t m, std::vector<std::string>& b) {
+            children.resize(m);
+            board = b;
+        }
+    };
 
-	/* Returns the index of the child node of root representing the best move
-	 * found.
-	 * @param root: tree representing current state
-	 */
-	const size_t alphaBeta(tree& root);
+    /* Returns the index of the child node of root representing the best move
+     * found.
+     * @param root: tree representing current state
+     */
+    const size_t alphaBeta(tree& root);
 
-	/* Returns the highest heuristic value for all children of current if we
-	 * look depth moves ahead
-	 * @param current: tree representing current state
-	 * @param depth: how many moves to look ahead
-	 * @param alpha: the current alpha value
-	 * @param beta: the current beta value
-	 */
-	const int maximize(tree& current, size_t depth, int alpha, int beta);
+    /* Returns the highest heuristic value for all children of current if we
+     * look depth moves ahead
+     * @param current: tree representing current state
+     * @param depth: how many moves to look ahead
+     * @param alpha: the current alpha value
+     * @param beta: the current beta value
+     */
+    const int maximize(tree& current, size_t depth, int alpha, int beta);
 
-	/* Exactly like maximize, except we're going for lowest heuristic value. */
-	const int minimize(tree& current, size_t depth, int alpha, int beta);
+    /* Exactly like maximize, except we're going for lowest heuristic value. */
+    const int minimize(tree& current, size_t depth, int alpha, int beta);
 
-	/* Heuristic function to return score of the given board 
-	 * @param board: the board you want scored
-	 */
-	const int scoreBoard(const std::vector<std::string>& board);
+    /* Heuristic function to return score of the given board 
+     * @param board: the board you want scored
+     */
+    const int scoreBoard(const std::vector<std::string>& board);
 
-	/* Did the most recent move result in a win?
-	 * @param board: the board you want evaluated
-	 * @param myTurn: did I just move?
-	 */
-	const bool winningMove(const std::vector<std::string>& board, const bool myTurn);
+    /* Did the most recent move result in a win?
+     * @param board: the board you want evaluated
+     * @param myTurn: did I just move?
+     */
+    const bool winningMove(
+        const std::vector<std::string>& board, const bool myTurn);
 
-	const std::vector<std::string> makeMove(
-		const std::vector<std::string>& board, const size_t move, const bool myTurn);
+    const std::vector<std::string> makeMove(
+        const std::vector<std::string>& board,
+        const size_t move,
+        const bool myTurn);
 
 }
 
